8c86b8af073fcdfc7753ed3cce96eb5bf68be85f
Gabriel Souto <ga.vs_@outlook.com>
2020-10-28 15:58:55 -0200

    add Exemplo de Busca Binária

---
+++ Ordenação/C/binary_search.c
@@ -0,0 +1,51 @@
+#include<stdio.h>
+#include<stdlib.h>
+#define len 1000
+
+void getData(int array[])
+{
+    char value[50];int i=0;
+    FILE* arq;
+    arq = fopen("binary_search_numbers.txt", "r");
+    while(fscanf(arq, "%s", value) != EOF)
+    {
+        array[i++] = atoi(value);
+    }
+}
+
+int sequential_search(int array[], int value)
+{
+    int i=0;
+    while(array[i]!=value)
+        i++;
+    printf("Sequential Search iterations: %d\n", i);
+    if((i==len || i == 0) && i == len) return -1;
+    return i;
+}
+
+int binary_search(int array[], int value)
+{
+    //l = left | r = right
+    int l = -1, r = len, i=0;
+    while(l < r-1)
+    {
+        int p = (l + r) / 2;
+        if(array[p] < value) l = p;
+        else r = p;
+        i++;
+    }
+    printf("Binary Search Iterations: %d\n", i);
+    if((r ==len || r == 0) && value != array[r]) return -1; //this function returns the len of array or 0 if the elemente don't exists in the same, this if solve this;
+    return r;
+}
+
+int main()
+{
+    int array[len], i;
+    getData(array);
+
+    int search = 999;
+    int bIndex = binary_search(array, search);
+    int sIndex = sequential_search(array, search);
+    printf("\n\nIndex of %d: %d\n", search, bIndex);
+}
