9a1bb16e3e12e84047b4c3bbaff2bbbd4cb81a70
Gabriel Souto <ga.vs_@outlook.com>
2020-11-03 23:02:58 -0200

    Create

--- Recursão/C/fibonacciRecursivo.c
+++ Recursão/C/fibonacciRecursivo.c
@@ -1,17 +1,16 @@
 #include<stdio.h>
 
 //RETORNA O N-ÉSIMO NÚMERO DA SEQUÊNCIA DE FIBONACCI
-//1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584, ...
-//1, 2, 3, 4, 5, 6,  7,  8,  9, 10, 11,  12,  13,  14,  15,  16, , 17,   18, ...
+
 int fib(int n)
 {
-    if(n >= 2)
-        return (fib(n-1) + fib(n-2));
-    return n;
+    if(n <= 1)
+        return 1;
+    return fib(n-1) + fib(n-2);
 }
 
 int main()
 {
-    int n = 14;
+    int n = 6;
     printf("fib(%d): %d\n", n, fib(n));
 }
