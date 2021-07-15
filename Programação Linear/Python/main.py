983f205834fcfa78dbfc33090a06e2543418b66d
gahvs <ga.vs_@outlook.com>
2021-07-15 10:00:23 -0300

    added somes methods

--- Programação Linear/Python/main.py
+++ Programação Linear/Python/main.py
@@ -1,14 +1,15 @@
 from vector.Vector import Vector, VectorComponent
 
 vector = Vector()
-vector.add(VectorComponent(index=100, number=0))
-vector.add(VectorComponent(index=200, number=0))
-vector.add(VectorComponent(index=300, number=0))
-vector.add(VectorComponent(index=400, number=0))
-vector.add(VectorComponent(index=500, number=0))
-vector.add(VectorComponent(index=600, number=0))
-vector.add(VectorComponent(index=700, number=0))
-vector.add(VectorComponent(index=800, number=0))
+vector.add(VectorComponent(index=100, number=1))
+vector.add(VectorComponent(index=200, number=2))
+vector.add(VectorComponent(index=300, number=3))
+vector.add(VectorComponent(index=400, number=4))
 
-print(vector.vector())
-print(vector.isNull())
+vector2 = Vector()
+vector2.add(VectorComponent(index=100, number=5))
+vector2.add(VectorComponent(index=200, number=6))
+vector2.add(VectorComponent(index=300, number=7))
+vector2.add(VectorComponent(index=400, number=8))
+
+Vector.copy
--- Programação Linear/Python/vector/Vector.py
+++ Programação Linear/Python/vector/Vector.py
@@ -77,3 +77,70 @@ class Vector:
             A vector is null if x[n] = 0 for all n in vector.indexes.
         """
         return all(map(lambda n: n == 0, self.numbers()))
+
+    def multiplyByEscalar(self, escalar) -> dict:
+        """
+            Returns the vector resulting from multiplying each component of a vector by a scalar.
+        """
+        for k in self.__vector:
+            self.__vector[k] *= escalar
+
+    def divideByEscalar(self, escalar) -> dict:
+        """    
+            Returns the vector resulting from dividing each component of a vector by a scalar.
+        """
+        for k in self.__vector:
+            self.__vector[k] /= escalar
+    
+    @staticmethod
+    def copy(vector):
+        """
+            Returns a copy, of the vector
+        """
+        copy = Vector()
+        for k, v in vector.vector().items():
+            copy.add(component=VectorComponent(index=k, number=v))
+        return copy
+
+    @staticmethod
+    def equalIndexSet(vec_x, vec_y) -> bool:
+        """
+            Returns true if vec_x and vex_y are defined on the same set of indices, and false otherwise.
+        """
+        return vec_x.indexes() == vec_y.indexes()
+
+    @staticmethod
+    def bigger(vec_x, vec_y) -> bool:
+        """
+            Returns True if vec_x[n] is greater than vec_y[n] for all n in vec_x.indexes.
+            This comparison only makes sense if both vectors are defined on the same set of indices.
+        """
+        v_x, v_y = vec_x.vector(), vec_y.vector()
+        return all(map(lambda k: v_x[k] > v_y[k], vec_x.indexes()))
+    
+    @staticmethod
+    def smaller(vec_x, vec_y) -> bool:
+        """
+            Returns True if vec_x[n] is less than vec_y[n] for all n in vec_x.indexes.
+            This comparison only makes sense if both vectors are defined on the same set of indices.
+        """
+        v_x, v_y = vec_x.vector(), vec_y.vector()
+        return all(map(lambda k: v_x[k] < v_y[k], vec_x.indexes()))
+
+    @staticmethod
+    def biggerOrEqual(vec_x, vec_y) -> bool:
+        """
+            Returns True if vec_x[n] is greater than or equal to vec_y[n] for all n in vec_x.indexes.
+            This comparison only makes sense if both vectors are defined on the same set of indices.
+        """
+        v_x, v_y = vec_x.vector(), vec_y.vector()
+        return all(map(lambda k: v_x[k] >= v_y[k], vec_x.indexes()))
+    
+    @staticmethod
+    def smallerOrEqual(vec_x, vec_y) -> bool:
+        """
+            Returns True if vec_x[n] is less than or equal to vec_y[n] for all n in vec_x.indexes.
+            This comparison only makes sense if both vectors are defined on the same set of indices.
+        """
+        v_x, v_y = vec_x.vector(), vec_y.vector()
+        return all(map(lambda k: v_x[k] <= v_y[k], vec_x.indexes()))
